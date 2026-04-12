0e0f0b946586a782a154c35fe86bac3e90ea2942
MTOP600 <dielmacarreras@gmail.com>
2026-04-12 20:15:03 -0300

    chore: copy the same models of the backend, his functions is the same

---
+++ frontend/src/models/product.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+    stock: { type: Number, required: true, default: 0 },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    image: { type: String, default: null },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Product", productSchema);
